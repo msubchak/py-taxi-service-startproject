76823bf069b541e72d6c2104c4ee646f739eafd9
Maksym Subchak <donjkee2@gmail.com>
2025-07-14 14:00:18 +0300

    Add Django project files and apps

---
+++ taxi/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import Manufacturer, Car, Driver
+
+
+@admin.register(Driver)
+class DriverAdmin(UserAdmin):
+    list_display = [
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "license_number",
+    ]
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("license_number",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+    ("Additional info", {"fields": ("license_number",)}),
+    )
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    search_fields = ["model"]
+    list_filter = ["manufacturer"]
+
+admin.site.register(Manufacturer)
+
